5b135812ab78f7de59e2f39737d73d85e1e98e9d
kimiaForuhar <kimyafruhr@gmail.com>
2021-12-03 21:54:36 +0330

    comments added

--- js/script.js
+++ js/script.js
@@ -1,18 +1,22 @@
-const error = document.querySelector(".error");
+const error = document.querySelector(".error"); //error div
+
+//adding a onclick function for submit button
 document.getElementById("submit").onclick = function (event) {
-  event.preventDefault();
-  input = document.getElementById("person-name").value;
+  event.preventDefault(); //prevent default submit function of javascript
+  input = document.getElementById("person-name").value; //input name value
+  //check that its a valid name or not
   if (validName(input)) {
-    document.getElementById("female").checked = false;
-    document.getElementById("male").checked = false;
-    var request = new XMLHttpRequest();
-    request.open("GET", `https://api.genderize.io/?name=${input}`, false);
+    document.getElementById("female").checked = false; //making radio button of female false
+    document.getElementById("male").checked = false; //making radio button of male false
+    var request = new XMLHttpRequest(); // making a new request
+    request.open("GET", `https://api.genderize.io/?name=${input}`, false); //open a request on api endPoint and make a GET request
     try {
       request.send(null);
     } catch (error) {
-      showErrorMessage(error);
+      showErrorMessage(error); //show any error if we cant send the request
     }
-    var response = JSON.parse(request.response);
+    var response = JSON.parse(request.response); //getting the response
+    //change the value of gender and probability after getting the response and change if we have any saved data
     document.getElementById("gender").innerHTML = response.gender;
     if (response.gender == null)
       document.getElementById("gender").innerHTML = "null";
@@ -23,10 +27,12 @@ document.getElementById("submit").onclick = function (event) {
     } else {
       document.getElementById("saved-name").innerHTML = "not saved";
     }
+    //if its not valid show an error
   } else {
     showErrorMessage("not a valid name");
   }
 };
+//adding a onclick function for save button
 document.getElementById("save").onclick = function (event) {
   input = document.getElementById("person-name").value;
   if (validName(input)) {
@@ -45,10 +51,13 @@ document.getElementById("save").onclick = function (event) {
     showErrorMessage("not a valid name");
   }
 };
+//adding a onclick function for clear button
 
 document.getElementById("clear").onclick = function (event) {
-  input = document.getElementById("person-name").value;
+  input = document.getElementById("person-name").value; //get value of input
+  //check that its a valid name or not
   if (validName(input)) {
+    //remove the saved data in a try catch block
     try {
       localStorage.removeItem(input);
       document.getElementById("saved-name").innerHTML = "not Saved";
@@ -60,11 +69,12 @@ document.getElementById("clear").onclick = function (event) {
     showErrorMessage("not a valid name");
   }
 };
-
+//check the name contains just a-z and A-Z and the length of it is valid
 function validName(input) {
   var re = /^[a-zA-Z]{1,255}$/;
   return re.test(input);
 }
+//giving the error block visibility to show in page for 4 seconds
 function showErrorMessage(message) {
   console.log(message);
   error.classList.add("active");
